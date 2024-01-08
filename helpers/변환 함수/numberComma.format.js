1c1eb15d1b2aad3a57196fa614f27189907b33cf
ohtaekwon <ohtk1119@gmail.com>
2024-01-08 09:29:58 +0900

    천의 단위로 콤마를 찍는 함수

---
+++ helpers/변환 함수/numberComma.format.js
@@ -0,0 +1,15 @@
+/**
+ * 숫자 천 단위마다 콤마(,)찍는 포멧팅해주는 함수
+ *
+ * 숫자타입 값이 입력시, 천 단위마다 콤마를 통해 분리를 해준다.
+ * ex) Input : 10000 / output : 10,000
+ * @param {*} number
+ *
+ */
+
+const onlyNumber = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g; // 1000단위를 체크하는 유효성 검사
+
+const { validations } = validateConstant;
+export default function numberCommaFormat(number) {
+  return number.toString().replace(onlyNumber, ",");
+}
